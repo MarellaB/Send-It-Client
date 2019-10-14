2e646fbe6cea07e9d073e483dd19223e947cb382
MarellaB <brandonmarella@gmail.com>
2019-10-14 13:09:07 -0400

    Initial Commit

---
+++ Send-It/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Send-It
+//
+//  Created by Brandon Marella on 10/14/19.
+//  Copyright © 2019 Allegory. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
